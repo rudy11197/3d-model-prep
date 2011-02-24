ba4c600d7a0295fb10fab0bce24587bac64ea69e
thatjcb <thatjcb@d4008710-154c-7bb7-2722-81d3b0d495a1>
2011-02-24 15:36:33 +0000

    Improved camera movement controls.  Added an options form.

---
+++ Engine/OptionsForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Engine
+{
+    public partial class OptionsForm : Form
+    {
+        public OptionsForm()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        // == Results == 
+        //
+        public float MovementSpeed
+        {
+            get { return (float)numericSpeed.Value; }
+            set { numericSpeed.Value = (decimal)value; }
+        }
+        //
+        //////////////////////////////////////////////////////////////////////
+
+    }
+}
