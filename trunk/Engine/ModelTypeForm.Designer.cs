dd07df6eda31cabbc6fda3f378c213588e416a81
thatjcb <thatjcb@d4008710-154c-7bb7-2722-81d3b0d495a1>
2011-02-28 09:55:09 +0000

    Model type form.

---
+++ trunk/Engine/ModelTypeForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Engine
+{
+    partial class ModelTypeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModelTypeForm));
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.comboType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(353, 383);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 53;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(272, 383);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 52;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // comboType
+            // 
+            this.comboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboType.FormattingEnabled = true;
+            this.comboType.Location = new System.Drawing.Point(59, 21);
+            this.comboType.Name = "comboType";
+            this.comboType.Size = new System.Drawing.Size(242, 21);
+            this.comboType.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Type:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelHelp);
+            this.groupBox1.Location = new System.Drawing.Point(15, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(413, 329);
+            this.groupBox1.TabIndex = 56;
+            this.groupBox1.TabStop = false;
+            // 
+            // labelHelp
+            // 
+            this.labelHelp.Location = new System.Drawing.Point(6, 16);
+            this.labelHelp.Name = "labelHelp";
+            this.labelHelp.Size = new System.Drawing.Size(401, 310);
+            this.labelHelp.TabIndex = 0;
+            this.labelHelp.Text = resources.GetString("labelHelp.Text");
+            // 
+            // ModelTypeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 418);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboType);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ModelTypeForm";
+            this.Text = "Model Type";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ComboBox comboType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelHelp;
+    }
+}
---
+++ trunk/Engine/ModelTypeForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Engine
+{
+    public partial class ModelTypeForm : Form
+    {
+        public ModelTypeForm()
+        {
+            InitializeComponent();
+            BuildList();
+            SetInitialText();
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        // == Results ==
+        //
+        public string ModelType
+        {
+            get { return (string)comboType.SelectedItem; }
+            set
+            {
+                // Add anything that does noes not already exist
+                if (!comboType.Items.Contains(value))
+                {
+                    comboType.Items.Add(value);
+                }
+                comboType.SelectedItem = value;
+            }
+        }
+        //
+        //////////////////////////////////////////////////////////////////////
+
+        //////////////////////////////////////////////////////////////////////
+        // == Setup ==
+        //
+        private void BuildList()
+        {
+            comboType.Items.Clear();
+            comboType.Items.Add(GlobalSettings.modelTypeCharacter);
+            comboType.Items.Add(GlobalSettings.modelTypeStructure);
+            comboType.Items.Add(GlobalSettings.modelTypeEquipLight);
+            comboType.Items.Add(GlobalSettings.modelTypeEquipSupport);
+            comboType.Items.Add(GlobalSettings.modelTypeEquipSmallArms);
+            comboType.Items.Add(GlobalSettings.modelTypeEquipGrenade);
+            comboType.Items.Add(GlobalSettings.modelTypeGearHead);
+            comboType.Items.Add(GlobalSettings.modelTypeGearAccessory);
+            comboType.Items.Add(GlobalSettings.modelTypeGearOther);
+        }
+
+        private void SetInitialText()
+        {
+            comboType.SelectedText = GlobalSettings.modelTypeStructure;
+        }
+        //
+        //////////////////////////////////////////////////////////////////////
+    }
+}
