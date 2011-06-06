4ef4deb4f3bda421194aca06c88d4ec3c92baa49
thatjcb <thatjcb@d4008710-154c-7bb7-2722-81d3b0d495a1>
2011-06-06 15:46:48 +0000

    Mid way through adding a merge animations clip form.

--- Engine/MainForm.Designer.cs
+++ Engine/MainForm.Designer.cs
@@ -93,6 +93,7 @@ namespace Engine
             this.buttonLarge = new System.Windows.Forms.Button();
             this.buttonSmall = new System.Windows.Forms.Button();
             this.modelViewerControl = new Engine.ModelViewerControl();
+            this.mergeToClipsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericLarge)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericSmall)).BeginInit();
@@ -111,7 +112,7 @@ namespace Engine
             this.ClipNamesComboBox});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 27);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -130,7 +131,7 @@ namespace Engine
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 23);
             this.fileToolStripMenuItem.Text = "&File";
             // 
             // LoadRigidModelMenu
@@ -227,7 +228,7 @@ namespace Engine
             this.light2EnabledToolStripMenuItem,
             this.reverseLightingToolStripMenuItem});
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
             this.viewToolStripMenuItem.Text = "&View";
             // 
             // yUpMenuItem
@@ -396,7 +397,7 @@ namespace Engine
             this.createStructureBoundsItem,
             this.optimiseBoundsItem});
             this.diabolicalToolStripMenuItem.Name = "diabolicalToolStripMenuItem";
-            this.diabolicalToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.diabolicalToolStripMenuItem.Size = new System.Drawing.Size(71, 23);
             this.diabolicalToolStripMenuItem.Text = "&Diabolical";
             // 
             // loadmodelItem
@@ -463,19 +464,20 @@ namespace Engine
             this.ToolsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.splitFBXMenuItem,
             this.openTakesToolStripMenuItem,
+            this.mergeToClipsMenuItem,
             this.toolStripSeparator5,
             this.SaveBoneMapMenu,
             this.SaveBindPoseMenuItem,
             this.toolStripSeparator8,
             this.optionsToolStripMenuItem});
             this.ToolsMenuItem.Name = "ToolsMenuItem";
-            this.ToolsMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.ToolsMenuItem.Size = new System.Drawing.Size(48, 23);
             this.ToolsMenuItem.Text = "&Tools";
             // 
             // splitFBXMenuItem
             // 
             this.splitFBXMenuItem.Name = "splitFBXMenuItem";
-            this.splitFBXMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.splitFBXMenuItem.Size = new System.Drawing.Size(230, 22);
             this.splitFBXMenuItem.Text = "&Split FBX files...";
             this.splitFBXMenuItem.ToolTipText = "Split FBX Model files to have only one take per file";
             this.splitFBXMenuItem.Click += new System.EventHandler(this.SplitFBXMenu_Click);
@@ -485,8 +487,8 @@ namespace Engine
             this.openTakesToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
             this.openTakesToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
             this.openTakesToolStripMenuItem.Name = "openTakesToolStripMenuItem";
-            this.openTakesToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
-            this.openTakesToolStripMenuItem.Text = "&Extract FBX Takes...";
+            this.openTakesToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.openTakesToolStripMenuItem.Text = "Batch &Extract Takes To Clips...";
             this.openTakesToolStripMenuItem.ToolTipText = "Load a list of animation takes from an FBX file and save them in a keyframe forma" +
     "t";
             this.openTakesToolStripMenuItem.Click += new System.EventHandler(this.OpenTakesMenu_Click);
@@ -494,13 +496,13 @@ namespace Engine
             // toolStripSeparator5
             // 
             this.toolStripSeparator5.Name = "toolStripSeparator5";
-            this.toolStripSeparator5.Size = new System.Drawing.Size(171, 6);
+            this.toolStripSeparator5.Size = new System.Drawing.Size(227, 6);
             // 
             // SaveBoneMapMenu
             // 
             this.SaveBoneMapMenu.Enabled = false;
             this.SaveBoneMapMenu.Name = "SaveBoneMapMenu";
-            this.SaveBoneMapMenu.Size = new System.Drawing.Size(174, 22);
+            this.SaveBoneMapMenu.Size = new System.Drawing.Size(230, 22);
             this.SaveBoneMapMenu.Text = "Save Bone &Map...";
             this.SaveBoneMapMenu.ToolTipText = "Save a list of bone names with their numeric index and thier parent bone";
             this.SaveBoneMapMenu.Click += new System.EventHandler(this.SaveBoneMapMenu_Click);
@@ -509,7 +511,7 @@ namespace Engine
             // 
             this.SaveBindPoseMenuItem.Enabled = false;
             this.SaveBindPoseMenuItem.Name = "SaveBindPoseMenuItem";
-            this.SaveBindPoseMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.SaveBindPoseMenuItem.Size = new System.Drawing.Size(230, 22);
             this.SaveBindPoseMenuItem.Text = "Save Bind &Pose...";
             this.SaveBindPoseMenuItem.ToolTipText = "Save the matrix of each bone while it is at rest";
             this.SaveBindPoseMenuItem.Click += new System.EventHandler(this.SaveBindPoseMenu_Click);
@@ -517,12 +519,12 @@ namespace Engine
             // toolStripSeparator8
             // 
             this.toolStripSeparator8.Name = "toolStripSeparator8";
-            this.toolStripSeparator8.Size = new System.Drawing.Size(171, 6);
+            this.toolStripSeparator8.Size = new System.Drawing.Size(227, 6);
             // 
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
             this.optionsToolStripMenuItem.Text = "&Options";
             this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
             // 
@@ -531,7 +533,7 @@ namespace Engine
             this.windowToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.helpToolStripMenuItem1});
             this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
-            this.windowToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(63, 23);
             this.windowToolStripMenuItem.Text = "&Window";
             // 
             // helpToolStripMenuItem1
@@ -647,7 +649,7 @@ namespace Engine
             this.modelViewerControl.IsAnimated = false;
             this.modelViewerControl.Light1Enabled = true;
             this.modelViewerControl.Light2Enabled = true;
-            this.modelViewerControl.Location = new System.Drawing.Point(0, 24);
+            this.modelViewerControl.Location = new System.Drawing.Point(0, 27);
             this.modelViewerControl.Name = "modelViewerControl";
             this.modelViewerControl.PauseInput = true;
             this.modelViewerControl.ReverseLighting = true;
@@ -658,6 +660,13 @@ namespace Engine
             this.modelViewerControl.TabIndex = 1;
             this.modelViewerControl.Text = "modelViewerControl";
             this.modelViewerControl.ViewUp = 1;
+            // 
+            // mergeToClipsMenuItem
+            // 
+            this.mergeToClipsMenuItem.Name = "mergeToClipsMenuItem";
+            this.mergeToClipsMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.mergeToClipsMenuItem.Text = "&Merge Animations To Clips...";
+            this.mergeToClipsMenuItem.Click += new System.EventHandler(this.mergeToClipsMenuItem_Click);
             // 
             // MainForm
             // 
@@ -757,6 +766,7 @@ namespace Engine
         private System.Windows.Forms.ToolStripMenuItem light1EnabledToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem light2EnabledToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem reverseLightingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeToClipsMenuItem;
 
     }
 }
---
+++ Engine/TakeExtractor/MergeClipForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AssetData;
+
+namespace Engine
+{
+    public partial class MergeClipForm : Form
+    {
+        public MergeClipForm()
+        {
+            InitializeComponent();
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // == Properties ==
+        //
+        private List<string> upperBodyBones = new List<string>();
+        public List<string> UpperBodyBones
+        {
+            get { return upperBodyBones; }
+            set 
+            { 
+                upperBodyBones = value;
+                PopulateUpperBodyBoneList();
+            }
+        }
+
+        private IDictionary<string, int> boneMap;
+        public IDictionary<string, int> BoneMap
+        {
+            set 
+            { 
+                boneMap = value;
+                PopulateBoneList();
+                PopulateUpperBodyBoneList();
+            }
+        }
+
+        private List<string> clipNames = new List<string>();
+        public List<string> ClipNames
+        {
+            set
+            {
+                clipNames = value;
+                PopulateClipNames();
+            }
+        }
+
+        public string UpperBodyClip
+        {
+            get { return comboUpper.Text; }
+        }
+
+        public string LowerBodyClip
+        {
+            get { return comboLower.Text; }
+        }
+        //
+        /////////////////////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////////////////////
+        // == Initialise ==
+        //
+        private void PopulateBoneList()
+        {
+            comboBones.Items.Clear();
+            comboBones.Items.AddRange(boneMap.Keys.ToArray());
+            if (comboBones.Items.Count > 0)
+            {
+                comboBones.SelectedIndex = 0;
+            }
+        }
+
+        private void PopulateClipNames()
+        {
+            comboUpper.Items.Clear();
+            comboUpper.Items.AddRange(clipNames.ToArray());
+            if (comboUpper.Items.Count > 0)
+            {
+                comboUpper.SelectedIndex = 0;
+            }
+            comboLower.Items.Clear();
+            comboLower.Items.AddRange(clipNames.ToArray());
+            if (comboLower.Items.Count > 0)
+            {
+                comboLower.SelectedIndex = 0;
+            }
+        }
+
+        private void ValidateUpperBonesList()
+        {
+            if (boneMap == null || boneMap.Count < 1)
+            {
+                return;
+            }
+            // Remove any bones that done exist in the bone map
+            for (int i = upperBodyBones.Count - 1; i >= 0; i--)
+            {
+                if (!boneMap.ContainsKey(upperBodyBones[i]))
+                {
+                    upperBodyBones.RemoveAt(i);
+                }
+            }
+        }
+
+        private void PopulateUpperBodyBoneList()
+        {
+            ValidateUpperBonesList();
+            string result = "";
+            for (int i = 0; i < upperBodyBones.Count; i++)
+            {
+                result += upperBodyBones[i] + ParseData.div;
+            }
+            textBones.Text = result;
+        }
+        //
+        /////////////////////////////////////////////////////////////////////
+
+    }
+}
