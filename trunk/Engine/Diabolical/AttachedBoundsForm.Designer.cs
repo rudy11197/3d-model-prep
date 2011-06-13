ad672e8acbab11c9b222bc314030ea7a3c615e93
thatjcb <thatjcb@d4008710-154c-7bb7-2722-81d3b0d495a1>
2011-06-13 14:58:17 +0000

    And now with the form I just added!

---
+++ trunk/Engine/Diabolical/AttachedBoundsForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Engine
+{
+    partial class AttachedBoundsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.positionOffset = new Engine.PositionControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBones = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboIDs = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericRadius = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericRadius)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericRadius);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboIDs);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.buttonUpdate);
+            this.groupBox1.Controls.Add(this.buttonRemove);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.comboBones);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.positionOffset);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 180);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bone Attached Bounding Spheres";
+            // 
+            // positionOffset
+            // 
+            this.positionOffset.DecimalPlaces = 2;
+            this.positionOffset.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.positionOffset.Location = new System.Drawing.Point(9, 109);
+            this.positionOffset.Maximum = new Microsoft.Xna.Framework.Vector3(30000F, 30000F, 30000F);
+            this.positionOffset.Minimum = new Microsoft.Xna.Framework.Vector3(-30000F, -30000F, -30000F);
+            this.positionOffset.Name = "positionOffset";
+            this.positionOffset.Size = new System.Drawing.Size(359, 26);
+            this.positionOffset.TabIndex = 6;
+            this.positionOffset.Value = new Microsoft.Xna.Framework.Vector3(0F, 0F, 0F);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Offset relative to the head of the bone:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bone:";
+            // 
+            // comboBones
+            // 
+            this.comboBones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBones.FormattingEnabled = true;
+            this.comboBones.Location = new System.Drawing.Point(50, 62);
+            this.comboBones.Name = "comboBones";
+            this.comboBones.Size = new System.Drawing.Size(148, 21);
+            this.comboBones.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(204, 28);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 9;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(285, 28);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 10;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(285, 146);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(230, 198);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 50;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(311, 198);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 51;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ID:";
+            // 
+            // comboIDs
+            // 
+            this.comboIDs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboIDs.FormattingEnabled = true;
+            this.comboIDs.Location = new System.Drawing.Point(35, 29);
+            this.comboIDs.Name = "comboIDs";
+            this.comboIDs.Size = new System.Drawing.Size(163, 21);
+            this.comboIDs.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Radius:";
+            // 
+            // numericRadius
+            // 
+            this.numericRadius.DecimalPlaces = 2;
+            this.numericRadius.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericRadius.Location = new System.Drawing.Point(78, 146);
+            this.numericRadius.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.numericRadius.Name = "numericRadius";
+            this.numericRadius.Size = new System.Drawing.Size(120, 20);
+            this.numericRadius.TabIndex = 7;
+            // 
+            // AttachedBoundsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 233);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "AttachedBoundsForm";
+            this.Text = "Attached Bounds";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericRadius)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private PositionControl positionOffset;
+        private System.Windows.Forms.ComboBox comboBones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ComboBox comboIDs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericRadius;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ trunk/Engine/Diabolical/AttachedBoundsForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AssetData;
+
+namespace Engine
+{
+    public partial class AttachedBoundsForm : Form
+    {
+        public AttachedBoundsForm()
+        {
+            InitializeComponent();
+            comboBones.SelectedIndexChanged += new EventHandler(comboBones_SelectedIndexChanged);
+            comboIDs.SelectedIndexChanged += new EventHandler(comboIDs_SelectedIndexChanged);
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // == Properties ==
+        //
+        private DiabolicalManager diabolicalForm;
+        public DiabolicalManager DiabolicalForm
+        {
+            set 
+            { 
+                diabolicalForm = value;
+                UpdateEnabled();
+            }
+        }
+
+        private IDictionary<string, int> boneMap;
+        public IDictionary<string, int> BoneMap
+        {
+            set
+            {
+                boneMap = value;
+                PopulateBoneList();
+            }
+        }
+
+        private List<AttachedSphere> attachedCurrent = new List<AttachedSphere>();
+        public List<AttachedSphere> AttachedBounds
+        {
+            get { return attachedCurrent; }
+            set 
+            { 
+                attachedCurrent.Clear();
+                attachedPrevious.Clear();
+                if (value.Count > 0)
+                {
+                    attachedCurrent.AddRange(value);
+                    attachedPrevious.AddRange(value);
+                }
+                PopulateIDs();
+            }
+        }
+        // Used to reset the originals if the form is cancelled
+        private List<AttachedSphere> attachedPrevious = new List<AttachedSphere>();
+        public List<AttachedSphere> PreviousBounds
+        {
+            get { return attachedPrevious; }
+        }
+        //
+        /////////////////////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////////////////////
+        // == Initialise ==
+        //
+        private const string unknownBoneName = "!! Unknown !!";
+        private void PopulateBoneList()
+        {
+            comboBones.Items.Clear();
+            comboBones.Items.AddRange(boneMap.Keys.ToArray());
+            comboBones.Items.Add(unknownBoneName);
+            if (comboBones.Items.Count > 0)
+            {
+                comboBones.SelectedIndex = 0;
+            }
+            PopulateIDs();
+        }
+
+        private void PopulateIDs()
+        {
+            if (comboBones.Items.Count < 1 ||
+                attachedCurrent.Count < 1)
+            {
+                return;
+            }
+            int currentID = comboIDs.SelectedIndex;
+            comboIDs.Items.Clear();
+            for (int a = 0; a < attachedCurrent.Count; a++)
+            {
+                AttachedSphere item = attachedCurrent[a];
+                string output = a.ToString() + " " + GetBoneName(item.BoneIndex);
+                comboIDs.Items.Add(output);
+            }
+            if (currentID >= 0 && currentID < comboIDs.Items.Count)
+            {
+                comboIDs.SelectedIndex = currentID;
+            }
+            GetCurrentData();
+        }
+
+        private void UpdateEnabled()
+        {
+            buttonAdd.Enabled = false;
+            buttonOK.Enabled = false;
+            buttonUpdate.Enabled = false;
+            if (comboBones.SelectedIndex != comboBones.Items.Count - 1 &&
+                diabolicalForm != null)
+            {
+                buttonAdd.Enabled = true;
+                buttonOK.Enabled = true;
+                buttonUpdate.Enabled = true;
+            }
+        }
+        //
+        /////////////////////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////////////////////
+        // == Changes ==
+        //
+        private void comboIDs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetCurrentData();
+        }
+
+        private void comboBones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckBoneName();
+        }
+
+        private void CheckBoneName()
+        {
+            if (comboBones.Items.Count > 1 && 
+                comboBones.SelectedIndex >= comboBones.Items.Count - 1)
+            {
+                // Avoid selecting unknown if possible
+                ShowBoneNameFromAttached();
+            }
+        }
+
+        private void ShowBoneNameFromAttached()
+        {
+            if (comboIDs.SelectedIndex < 0 || 
+                comboIDs.SelectedIndex >= attachedCurrent.Count ||
+                comboBones.Items.Count < 1)
+            {
+                return;
+            }
+            if (attachedCurrent[comboIDs.SelectedIndex].BoneIndex >= 0 &&
+                attachedCurrent[comboIDs.SelectedIndex].BoneIndex < comboBones.Items.Count)
+            {
+                comboBones.SelectedIndex = attachedCurrent[comboIDs.SelectedIndex].BoneIndex;
+            }
+            else
+            {
+                // Unknown
+                comboBones.SelectedIndex = comboBones.Items.Count - 1;
+            }
+            UpdateEnabled();
+        }
+
+        private string GetBoneName(int boneID)
+        {
+            if (boneID >= 0 && boneID < comboBones.Items.Count)
+            {
+                return (string)comboBones.Items[boneID];
+            }
+            return unknownBoneName;
+        }
+
+        private void GetCurrentData()
+        {
+            if (comboIDs.SelectedIndex < 0 || 
+                comboIDs.SelectedIndex >= attachedCurrent.Count)
+            {
+                return;
+            }
+            ShowBoneNameFromAttached();
+            AttachedSphere item = attachedCurrent[comboIDs.SelectedIndex];
+            positionOffset.Value = item.Offset;
+            numericRadius.Value = (decimal)item.Sphere.Radius;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateChanges();
+        }
+
+        private void UpdateChanges()
+        {
+            if (diabolicalForm == null ||
+                comboIDs.SelectedIndex < 0 || 
+                comboIDs.SelectedIndex >= attachedCurrent.Count ||
+                comboBones.Items.Count < 2 ||
+                comboBones.SelectedIndex >= comboBones.Items.Count - 1)
+            {
+                return;
+            }
+            AttachedSphere item = attachedCurrent[comboIDs.SelectedIndex];
+            item.BoneIndex = comboBones.SelectedIndex;
+            item.Offset = positionOffset.Value;
+            item.Sphere.Radius = (float)numericRadius.Value;
+            attachedCurrent[comboIDs.SelectedIndex] = item;
+            // Update the model
+            diabolicalForm.AttachedBounds = attachedCurrent;
+            // Reload all the data
+            PopulateIDs();
+        }
+        //
+        /////////////////////////////////////////////////////////////////////
+
+    }
+}
